6c1354bf07bb1cef1fba3d966112d3dbdcce9019
nas <y.softvalley@hey.com>
2024-11-19 22:53:48 +0900

    ページコンポーネントのparamsをPromise型から解決したオブジェクトに変更し、非同期処理を整理（パラメータの取得方法の改善のため）

--- src/app/(conents)/[slug]/page.tsx
+++ src/app/(conents)/[slug]/page.tsx
@@ -12,9 +12,10 @@ export const generateStaticParams = async () => {
 };
 
 export const generateMetadata = async (
-  { params }: { params: { slug: string } },
+  props: { params: Promise<{ slug: string }> },
   parent: ResolvingMetadata,
 ): Promise<Metadata> => {
+  const params = await props.params;
   const content = await getContentBySlug(params.slug);
   if (!content) {
     return {
@@ -32,54 +33,50 @@ type Props = {
   params: Promise<{ slug: string }>;
 };
 
-export default async function Page({ params }: Props) {
+export default async function Page({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const { slug } = await params;
+
+  const content = await getContentBySlug(slug);
+  if (!content) {
+    return notFound();
+  }
+
+  return (
+    <div className="w-full max-w-xl">
+      <div className="relative w-full max-w-screen-xl">
+        <Image
+          src={content.imageUrl}
+          alt={content.title}
+          width={1080}
+          height={1920}
+          className="w-full h-auto object-contain"
+          priority
+          placeholder="blur"
+          blurDataURL={"/blur.png"}
+        />
+        <div className="py-2">
+          <div className="text-xs font-mono">
+            {content.imageTitle}
+            {content.imageArtistDisplayName !== "Unknown" &&
+              ` by ${content.imageArtistDisplayName}`}
+          </div>
+          <div className="text-xs font-mono">{content.imageObjectDate}</div>
+        </div>
+      </div>
+      <div className="border-y border-black border-t-2 py-2 md:my-8">
+        <p className="text-sm text-gray-600 font-thin underline capitalize">
+          {content.category}
+        </p>
+        <h1 className="font-bold my-2">{content.title}</h1>
+        <p className="text-gray-600 text-sm whitespace-nowrap">
+          {content.datetime.toISOString()}
+        </p>
+      </div>
+      <Content source={content.content} />
+    </div>
+  );
 }
-
-// export default async function Page({
-//   params,
-// }: {
-//   params: { slug: string };
-// }) {
-//   const { slug } = await params;
-
-//   const content = await getContentBySlug(slug);
-//   if (!content) {
-//     return notFound();
-//   }
-
-//   return (
-//     <div className="w-full max-w-xl">
-//       <div className="relative w-full max-w-screen-xl">
-//         <Image
-//           src={content.imageUrl}
-//           alt={content.title}
-//           width={1080}
-//           height={1920}
-//           className="w-full h-auto object-contain"
-//           priority
-//           placeholder="blur"
-//           blurDataURL={"/blur.png"}
-//         />
-//         <div className="py-2">
-//           <div className="text-xs font-mono">
-//             {content.imageTitle}
-//             {content.imageArtistDisplayName !== "Unknown" &&
-//               ` by ${content.imageArtistDisplayName}`}
-//           </div>
-//           <div className="text-xs font-mono">{content.imageObjectDate}</div>
-//         </div>
-//       </div>
-//       <div className="border-y border-black border-t-2 py-2 md:my-8">
-//         <p className="text-sm text-gray-600 font-thin underline capitalize">
-//           {content.category}
-//         </p>
-//         <h1 className="font-bold my-2">{content.title}</h1>
-//         <p className="text-gray-600 text-sm whitespace-nowrap">
-//           {content.datetime.toISOString()}
-//         </p>
-//       </div>
-//       <Content source={content.content} />
-//     </div>
-//   );
-// }
